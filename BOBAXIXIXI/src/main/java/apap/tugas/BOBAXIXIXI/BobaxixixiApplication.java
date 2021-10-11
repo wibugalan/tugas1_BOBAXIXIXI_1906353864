f858f7998855ce6c81b4ae073ab927c5d7ec7774
marsha.prawira <marsha.prawira@pinhome.id>
2021-10-11 23:24:50 +0700

    feat: init

---
+++ BOBAXIXIXI/src/main/java/apap/tugas/BOBAXIXIXI/BobaxixixiApplication.java
@@ -0,0 +1,12 @@
+package apap.tugas.BOBAXIXIXI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobaxixixiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobaxixixiApplication.class, args);
+	}
+}
---
+++ BOBAXIXIXI/src/test/java/apap/tugas/BOBAXIXIXI/BobaxixixiApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas.BOBAXIXIXI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobaxixixiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
